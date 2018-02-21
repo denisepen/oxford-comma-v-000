e88fc5353d23b21c84b5e37858edbf5501b800fe
Denise Pendleton <dpendl1@hotmail.com>
2018-02-21 01:29:20 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -12,6 +12,12 @@
 # end
 
 def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(",")
+  else
   sentence = array[0..-2].join(", ") + ", and " + array[-1]
+end
  sentence
 end
